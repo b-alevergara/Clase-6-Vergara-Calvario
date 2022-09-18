2f52510a6596196a5c80fde61b5f173ea06688cb
b-alevergara <belenvergara97@gmail.com>
2022-09-18 14:46:02 -0500

    ultimas correcciones en mis funciones y arrays

--- pruebas-correcciones.js
+++ pruebas-correcciones.js
@@ -18,6 +18,20 @@ alert('Eres menor de edad')
 for(let i=0; i<101; i++){
 alert('Eres la persona numero '+ i + ' en ingresar a la pagina')
 }
+class producto {
+    constructor(unidad, nombre, precio, cantidad) {
+        this.unidad = unidad;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.cantidad = cantidad;
+    }
+    costo=() => this.precio * this.cantidad; //revisar esto
+}
+const producto1 = new producto("Kilo de ", "Caucho granulado ", "$450", 1);
+return producto1;
+const producto2 = new producto("Kilo de ", "Caucho pulverizado ", "$650", 1);
+return producto2;
+
 let opciones 
     do{
         opciones = parseInt(prompt('Selecciona el producto a comprar: \n1.-Caucho Granulado \n2.-Salir'))}
@@ -33,9 +47,12 @@ let opciones
                 alert('Opción inválida')
 }
 }    
-
 let precio = 450;
-
+// function sumarProductos(precios){
+//     return precios.reduce((a,b) => a+b)
+// }
+// console.log(precios)
+// console.log(sumarProductos(precios))
 function calculoIva(arr){
     let precio = 450
     return arr.map(precio*0.16)
@@ -44,6 +61,12 @@ function calculoIva(arr){
 function calculoTotal(arr){
     return arr.reduce((a,b) => a+b)
 }       console.log(calculoTotal)
+// console.log(calculoTotal((sumarProductos(precios)),(calculoIva(precios))))
+//no estoy segura que este console log funcione
+// alert(`El costo total de tu pedido es de $ ${calculoTotal((sumarProductos(precios)),(calculoIva(precios)))}`)
 
+// sumarProductos(450, 650)
 calculoTotal( 450, calculoIva)
+// alert(`El costo de los productos seleccionados es $ ${sumarProductos}`)
 alert(`El total de su compra es de $ ${calculoTotal}`)
+
